1f17fbffc728be3b52327cac64926bda74390a0a
Tigggas <102190734+Tigggas@users.noreply.github.com>
2022-05-10 18:01:05 -0300

    Add files via upload

---
+++ Pro-C19-AA1-main/Pro-C19-AA1-main/sketch.js
@@ -0,0 +1,13 @@
+var estudante;
+function setup() {
+  createCanvas(400,400);
+estudante = new Student("Tiago",12,7);
+estudante.mostrar();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
+
---
+++ Pro-C19-AA1-main/Pro-C19-AA1-main/student.js
@@ -0,0 +1,21 @@
+
+class Student
+
+{       
+        constructor(nome,idade, serie)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.serie = serie;       
+        }
+    
+        mostrar()
+        {
+          console.log( this.nome);
+          console.log( this.idade);
+          console.log( this.serie);
+        }
+}
+
+
+
